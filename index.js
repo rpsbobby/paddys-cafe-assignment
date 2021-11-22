e0b5fe1f3d5bb60e1564b84530e4937e4015e58c
Robert Szlufik <robert.szlufik123@gmail.com>
2021-11-22 16:33:33 +0000

    server set

--- index.js
+++ index.js
@@ -5,3 +5,16 @@ const   http = require('http'), // this module provides HTTP server funcions
         xmlParse = require('xslt-processor').xmlParse,
         xsltProcess = require('xslt-processor').xsltProcess,
         xml2js = require('xml2js');
+
+
+const router = express(),
+    server = http.createServer(router);
+
+    router.get("/", function(req, res) {
+        
+    })
+
+    server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0.", function() {
+            const addr = server.address();
+            console.log("Server listening at: ", addr.address , ": " , addr.port)
+    })
